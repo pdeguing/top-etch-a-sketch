5609146149fb16ac1f0ea5bdd3658d4989f74019
Pierre-Quentin Deguingand <39034711+pdeguing@users.noreply.github.com>
2024-12-22 20:47:02 +0100

    oops I commit all at once

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector("#container");
+let gridSize = 16;
+
+function buildGrid(size) {
+  container.replaceChildren();
+  for (let i = 0; i < size * size; i++) {
+    const newDiv = document.createElement("div");
+
+    newDiv.className = "square";
+    newDiv.style.flexBasis = `calc(100% / ${gridSize})`;
+    newDiv.style.maxWidth = `calc(100% / ${gridSize})`;
+    container.appendChild(newDiv);
+  }
+
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", changeColorOnHover);
+  });
+}
+
+buildGrid(gridSize);
+
+function changeColorOnHover(event) {
+  event.target.style.backgroundColor = "darkgrey"; // e.g., 'red', '#FF0000', 'rgba(255, 0, 0, 0.5)'
+}
+
+const button = document.querySelector("#new");
+
+button.addEventListener("click", () => {
+  gridSize = prompt("please enter a grid size number (max 100)");
+
+  if (gridSize <= 0 || gridSize > 100) gridSize = 16;
+  buildGrid(gridSize);
+});
